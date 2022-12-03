cf59725d141dc9571bc70575d0b3b42c25f459af
Ambrosini Nico (IT-PTR-CEN1-BDE4) <nico.ambrosini@sbb.ch>
2022-12-03 18:45:38 +0100

    Init 2021

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# aoc-2021
+# AdventOfCode 2021
+
+[![CI](https://github.com/nambrosini/aoc-2021/actions/workflows/fmt-test-cov.yml/badge.svg)](https://github.com/nambrosini/aoc-2021/actions/workflows/fmt-test-cov.yml)
+[![codecov](https://codecov.io/gh/nambrosini/aoc-2021/branch/main/graph/badge.svg?token=eTmYF9qEKz)](https://codecov.io/gh/nambrosini/aoc_2021)
+
+My solution for the 2021 AdventOfCode.
---
+++ src/day02.rs
@@ -0,0 +1,84 @@
+#[aoc_generator(day2)]
+pub fn generator(input: &str) -> Vec<Direction> {
+    input.lines().map(|line| line.into()).collect()
+}
+
+#[aoc(day2, part1)]
+pub fn part1(input: &[Direction]) -> usize {
+    let mut h = 0;
+    let mut v = 0;
+
+    for d in input {
+        match d {
+            Direction::Forward(k) => h += k,
+            Direction::Up(k) => v -= k,
+            Direction::Down(k) => v += k,
+        }
+    }
+
+    h * v
+}
+
+#[aoc(day2, part2)]
+pub fn part2(input: &[Direction]) -> usize {
+    let mut aim = 0;
+    let mut h = 0;
+    let mut v = 0;
+
+    for d in input {
+        match d {
+            Direction::Forward(k) => {
+                h += k;
+                v += aim * k;
+            }
+            Direction::Up(k) => aim -= k,
+            Direction::Down(k) => aim += k,
+        }
+    }
+
+    h * v
+}
+
+pub enum Direction {
+    Forward(usize),
+    Down(usize),
+    Up(usize),
+}
+
+impl From<&str> for Direction {
+    fn from(d: &str) -> Direction {
+        let split: Vec<&str> = d.split(' ').collect();
+        let v: usize = split[1].parse().unwrap();
+
+        match split[0] {
+            "forward" => Direction::Forward(v),
+            "up" => Direction::Up(v),
+            "down" => Direction::Down(v),
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[test]
+fn test1() {
+    let s = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+
+    assert_eq!(part1(&generator(s)), 150);
+}
+
+#[test]
+fn test2() {
+    let s = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+
+    assert_eq!(part2(&generator(s)), 900);
+}
